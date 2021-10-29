8e07595c7f892ba23c32d168c5c29d2c673ec9d4
Germiprogramer <llorentemunozgerman@gmail.com>
2021-10-29 09:49:21 +0200

    mi septimo commit

---
+++ PROGRAMACION TAREA 1 ejercicio 2.py
@@ -0,0 +1,5 @@
+from colorama import init, Fore
+
+init()
+
+print (Fore.GREEN + "hola mundo")
---
+++ PROGRAMACIÓN TAREA 1 ejercicio 5.py
@@ -0,0 +1,13 @@
+dinero_inicial = 2000
+hambre = 100
+
+if dinero_inicial >= 1700 and dinero_inicial <= 2000:
+   print("no tengo hambre")
+precio_helado = 100
+
+precio_helado = 100
+def compro_helado():
+    dinero -= precio_helado
+    precio_helado *= 1.2
+
+
