22ae0ed642d13780679275d6d0a9d1655929fc45
Germiprogramer <91720991+Germiprogramer@users.noreply.github.com>
2021-10-29 09:16:52 +0200

    Add files via upload

---
+++ PROGRAMACIÓN TAREA 1 ejercicio 3.py
@@ -0,0 +1,11 @@
+num1 = 21
+num2 = 43
+print(num1 + num2)
+num3 = 142
+num4 = 52
+print(num3 - num4)
+num5 = 10
+num6 = 342
+print(num5 * num6)
+num7 = 5
+print(num7**2)
---
+++ shipping.py
@@ -0,0 +1,22 @@
+#tarea6
+customer_basket_cost = 34
+customer_basket_weight = 44
+
+if customer_basket_cost > 100:
+    precio_envio = 0
+else:
+    precio_envio = 1.2 * customer_basket_weight
+
+coste_total = (customer_basket_cost + customer_basket_weight + precio_envio)
+print(coste_total)
+
+customer_basket_cost = 101
+customer_basket_weight = 44
+
+if customer_basket_cost > 100:
+    precio_envio = 0
+else:
+    precio_envio = 1.2 * customer_basket_weight
+
+coste_total = (customer_basket_cost + customer_basket_weight + precio_envio)
+print(coste_total)
